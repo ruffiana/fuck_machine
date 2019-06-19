162d270822b12326e1bb320e1c0de1cea3b29a14
ruffiana <51935769+ruffiana@users.noreply.github.com>
2019-06-18 21:24:01 -0500

    Update app.js
    Modified to work with index.html

--- js/app.js
+++ js/app.js
@@ -1,41 +1,48 @@
-(function(){
+alert( 'Script Running...' );
 
-	var settings = {
-		channel: 'Channel-Ruffiana_Plays',
-		publish_key: 'pub-c-486a9fac-91fa-4b7a-b3e3-672a80ca88a4',
-		subscribe_key: 'sub-c-6bbe07d2-921f-11e9-b271-9aa5f5ecb24a'
-	};
+var settings = {
+	channel: 'Channel-Ruffiana_Plays',
+	publish_key: 'pub-c-486a9fac-91fa-4b7a-b3e3-672a80ca88a4',
+	subscribe_key: 'sub-c-6bbe07d2-921f-11e9-b271-9aa5f5ecb24a'
+};
 
-	var pubnub = PUBNUB(settings);
-
-	var speed = document.getElementById('slider-speed');
-
-	pubnub.subscribe({
-		channel: settings.channel,
-		callback: function(m) {
-		}
-	})
-
-	/* 
-		Data settings:
-
-		LED
-
-		item: 'light-*'
-		speed: 0 - 10
-
-	*/
-
-	function publishUpdate(data) {
-		pubnub.publish({
-			channel: settings.channel, 
-			message: data
-		});
+var pubnub = new PubNub(
+	{
+		channels: [settings.channel],
+		publishKey: settings.publish_key,
+		subscribeKey: settings.subscribe_key
 	}
+);
 
-	// UI EVENTS
+var speedSlider = document.getElementById('speedSlider');
 
-	speed.addEventListener('change', function(e){
-		publishUpdate({item: 'slider-speed', speed: +this.value});
-	}, false);
-})();
+// pubnub.subscribe({
+// 	channel: settings.channel,
+// 	callback: function(m) {
+// 	}
+// })
+
+/* 
+	Data settings:
+
+	LED
+
+	item: 'light-*'
+	speed: 0 - 10
+
+*/
+
+function publishUpdate(data) {
+	pubnub.publish(
+		{
+			message: data,
+			channel: settings.channel
+		}
+	);
+}
+
+// UI EVENTS
+
+speedSlider.addEventListener('change', function(e){
+	publishUpdate({item: "speedSlider", speed: + this.value});
+}, false);
