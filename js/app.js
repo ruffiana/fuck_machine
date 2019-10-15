03e26fdfe4d7e5f85eec60e7fbf0dfe23bc31ce2
ruffiana <51935769+ruffiana@users.noreply.github.com>
2019-10-15 15:39:53 -0500

    Update app.js
    added functionality for button changes

--- js/app.js
+++ js/app.js
@@ -12,21 +12,16 @@ var pubnub = new PubNub(
 	}
 );
 
-// pubnub.subscribe({
-// 	channel: settings.channel,
-// 	callback: function(m) {
+// pubnub.subscribe(
+// 	{
+// 		channel: settings.channel,
+// 		callback: subscriptUpdate(m)
 // 	}
-// })
+// );
 
-/* 
-	Data settings:
+// function subscriptUpdate(data) {
 
-	LED
-
-	item: 'light-*'
-	speed: 0 - 10
-
-*/
+// };
 
 function publishUpdate(data) {
 	pubnub.publish(
@@ -35,20 +30,65 @@ function publishUpdate(data) {
 			channel: settings.channel
 		}
 	);
-}
+};
 
 // UI EVENTS
-var speedSlider = document.getElementById('speedSlider');
+/* video */
 var vid = document.getElementById('vid');
-vid.playbackRate = 0.0
+vid.playbackRate = 0.0;
 
-function setVideoSpeed(obj)
-{
-	var value = obj.value;
-		vid.playbackRate = 5.0 * (obj.value / 10.0);
-}
+/* slider bar */
+// var speedSlider = document.getElementById('speedSlider');
 
-speedSlider.addEventListener('change', function(e){
-	publishUpdate({item: "speedSlider", speed: + this.value});
-	setVideoSpeed(speedSlider);
-}, false);
+// speedSlider.addEventListener('change', onSliderChanged(this.value), false);
+// function onSliderChanged(val) {
+// 	// publishUpdate({item: "speedSlider", speed: val});
+// 	setSpeed(val);
+// };
+
+/* speed buttons */
+var button0 = document.getElementById('button0');
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+var button4 = document.getElementById('button4');
+var button5 = document.getElementById('button5');
+var button6 = document.getElementById('button6');
+var button7 = document.getElementById('button7');
+var button8 = document.getElementById('button8');
+var button9 = document.getElementById('button9');
+var button10 = document.getElementById('button10');
+
+button0.onclick = function() {onButtonClick(0.0)};
+button1.onclick = function() {onButtonClick(1.0)};
+button2.onclick = function() {onButtonClick(2.0)};
+button3.onclick = function() {onButtonClick(3.0)};
+button4.onclick = function() {onButtonClick(4.0)};
+button5.onclick = function() {onButtonClick(5.0)};
+button6.onclick = function() {onButtonClick(6.0)};
+button7.onclick = function() {onButtonClick(7.0)};
+button8.onclick = function() {onButtonClick(8.0)};
+button9.onclick = function() {onButtonClick(9.0)};
+button10.onclick = function() {onButtonClick(10.0)};
+
+function onButtonClick(val) {
+	// publishUpdate({item: "speedButton", speed: val});
+
+	// This is set directly here for testing but should be set by a 
+	// pubnub subcribe message from the remote client
+	setSpeed(val);
+};
+
+function setSpeed(val) {
+	// sets color and background color of button elements based on speed
+	for (i = 0; i <= val; i++) {
+		document.getElementById('button'+i).style.color = "#f2a0c0";
+		document.getElementById('button'+i).style.backgroundColor = "#ec6597";
+	};
+	for (i = val+1; i <= 10; i++) {
+		document.getElementById('button'+i).style.color = "#993333";
+		document.getElementById('button'+i).style.backgroundColor = "#331111";
+	};
+		
+	vid.playbackRate = 5.0 * (val / 10.0);
+};
