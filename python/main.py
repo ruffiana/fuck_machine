6f80ca1dde619b2d7926d6d2c635a04b107480b1
ruffiana <51935769+ruffiana@users.noreply.github.com>
2019-10-18 08:34:19 -0500

    Update main.py
    Added handlers for subscribe listener requests

--- python/main.py
+++ python/main.py
@@ -1,82 +1,161 @@
 # coding: utf-8
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+import sys
+from time import sleep
+
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNOperationType, PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+
+from config import SUBSCRIBE_KEY, PUBLISH_KEY, CHANNEL
+from digipot import Speed_Control
 
 
 
-class Speed_Control():
-    """
-    Class to handle PWM through MCP4131 chip via GPIO on raspberry pi 3
+# PubNub setup
+pnconfig = PNConfiguration() 
+pnconfig.subscribe_key = SUBSCRIBE_KEY
+pnconfig.publish_key = PUBLISH_KEY
 
-    ``:Examples:``
 
-        # instantiate a Speed_Control object
-        speed_control = Speed_Control()
 
-        # set speed via 
-        speed_control.set_speed(5)
 
-        # sets speed to 0 and resets GPIO before quitting
-        speed_control.cleanup()
+def Publish_Listener(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
 
-    ``ToDo``
-        implement @property for speed
 
-    """
-    # Rasberry Pi 3 pins
-    # GPIO.BCM mode uses Broadcom SOC channel
-    # GPIO## not pin#
-    SPI_CS_PIN = 8 #SPI_CE0_N
-    SPI_CLK_PIN = 11 # SPI_CLK
-    SPI_SDISDO_PIN = 10 # SPI_MOSI
+
+# Listener to handle messages and events
+class Subscribe_Listener(SubscribeCallback):
+    MAX_SPEED = 10
     
-    def __init__(self, speed = 0):
-        # define GPIO settings
-        GPIO.setwarnings(False)         # do not show any warnings
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.SPI_CS_PIN, GPIO.OUT)
-        GPIO.setup(self.SPI_CLK_PIN, GPIO.OUT)
-        GPIO.setup(self.SPI_SDISDO_PIN, GPIO.OUT)
+    def __init__(self):
+        self.speed_control = Speed_Control()
+    
+
+    def status(self, pubnub, status):
+        # The status object returned is always related to subscribe but could contain
+        # information about subscribe, heartbeat, or errors
+        # use the operationType to switch on different options
+        if status.operation == PNOperationType.PNSubscribeOperation \
+                or status.operation == PNOperationType.PNUnsubscribeOperation:
+            if status.category == PNStatusCategory.PNConnectedCategory:
+                # This is expected for a subscribe, this means there is no error or issue whatsoever
+                print(f'Connected to {CHANNEL}')
+                msg = {'statusOnline': True}
+                pubnub.publish().channel(CHANNEL).message(msg).sync()
+            elif status.category == PNStatusCategory.PNReconnectedCategory:
+                # This usually occurs if subscribe temporarily fails but reconnects. This means
+                # there was an error but there is no longer any issue
+                print(f'Re-connected to {CHANNEL}')
+            elif status.category == PNStatusCategory.PNDisconnectedCategory:
+                # This is the expected category for an unsubscribe. This means there
+                # was no error in unsubscribing from everything
+                pass
+            elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                # This is usually an issue with the internet connection, this is an error, handle
+                # appropriately retry will be called automatically
+                pubnub.reconnect()
+            elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+                # This means that PAM does not allow this client to subscribe to this
+                # channel and channel group configuration. This is another explicit error
+                pass
+            else:
+                # This is usually an issue with the internet connection, this is an error, handle appropriately
+                # retry will be called automatically
+                pass
+        elif status.operation == PNOperationType.PNSubscribeOperation:
+            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
+            # For more information on how to configure heartbeat notifications through the status
+            # PNObjectEventListener callback, consult http://www.pubnub.com/docs/python/api-reference-configuration#configuration
+            if status.is_error():
+                # There was an error with the heartbeat operation, handle here
+                pass
+            else:
+                # Heartbeat operation was successful
+                pass
+        else:
+            # Encountered unknown status type
+            pass
+
+
+    def message(self, pubnub, message):
+        """
+        Event handlers for pubNub messages
         
-        # set initial speed to 0
-        self._speed = speed
+        Arguments:
+            pubnub {[type]} -- [description]
+            message {[type]} -- [description]
+        """
+        _message = message.message
+        _keys = _message.keys()
+
+        if 'requestOnlineStatus' in _keys:
+            msg = {'statusOnline': True}
+            pubnub.publish().channel(CHANNEL).message(msg).sync()
+
+        if 'requestSpeedCurrent' in _keys:
+            msg = {'speedCurrent': self.speed_control.speed}
+            pubnub.publish().channel(CHANNEL).message(msg).sync()
+
+        if 'requestSpeedChange' in _keys:
+            new_speed = _message['requestSpeedChange']
+            self.speed_control.speed = new_speed
+            msg = {'speedCurrent': self.speed_control.speed}
+            pubnub.publish().channel(CHANNEL).message(msg).sync()
 
 
-    @property
-    def speed( self ):
-        #print(f'Current Speed is {self._speed}')
-        return self._speed
+    def presence(self, pubnub, presence):
+        pass
 
 
-    @speed.setter
-    def speed(self, speed, min_speed=0, max_speed=10, max_out=127):
-        #print( f'speed:{speed}, min_speed={min_speed}, max_speed={max_speed}, max_out={max_out}')
-        # make sure given speed is between 0 and 10
-        speed = max(min_speed, min(speed, max_speed))
-        self._speed = speed
+
+def main( ):
+    try:
+        print('Initializing PubNub...')
+        pubnub = PubNub(pnconfig)
         
-        # Scale speed to a range of 0-127
-        speed = int(speed * (max_out / max_speed))
+        print('Setting up listeners...')
+        my_listener = Subscribe_Listener()
+        pubnub.add_listener(my_listener)
+
+        print(f'Subscribing to {CHANNEL}...')
+        pubnub.subscribe().channels(CHANNEL).execute()
+
+        print('Active. Press CTRL-C to exit.')
+        while True:
+            pass
+    except KeyboardInterrupt:
+        # here you put any code you want to run before the program   
+        # exits when you press CTRL+C
+        print('Exiting...')
+    except:  
+        # this catches ALL other exceptions including errors.  
+        # You won't get any error messages for debugging  
+        # so only use it once your code is working  
+        print( "Other error or exception occurred!" )
+    finally:
+        msgs = [
+            {'speedCurrent': 0},
+            {'statusOnline': False}
+        ]
+        for m in msgs:
+            pubnub.publish().channel(CHANNEL).message(m).sync()
         
-        # send scaled speed value through GPIO to digipot
-        GPIO.output(self.SPI_CS_PIN, True)
-
-        GPIO.output(self.SPI_CLK_PIN, False)
-        GPIO.output(self.SPI_CS_PIN, False)
-
-        b = '{0:016b}'.format(speed)
-        for x in range(0, 16):
-            #print('x:' + str(x) + ' -> ' + str(b[x]))
-            GPIO.output(self.SPI_SDISDO_PIN, int(b[x]))
-
-            GPIO.output(self.SPI_CLK_PIN, True)
-            GPIO.output(self.SPI_CLK_PIN, False)
-
-        GPIO.output(self.SPI_CS_PIN, True)
-        
-        #print(f'Speed changed to {self._speed}')
+        pubnub.unsubscribe_all()
+        my_listener.speed_control.cleanup()
+        sys.exit(1)
 
 
-    def cleanup(self):
-        self.speed = 0
-        GPIO.cleanup() # this ensures a clean exit
+
+# if __name__ == 'main':
+#     main()
+
+main()
