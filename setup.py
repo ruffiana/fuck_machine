5e899a24849e1be326255d0ea1fba9d7f92e7cfb
Ruffiana <ruffiana.plays@gmail.com>
2021-02-15 16:33:08 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/ruffiana/SissyUniversity/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='fuck_machine',
+    version='1.0.0',
+    description='HTML interface for speed control via RPI',
+    long_description=readme,
+    author='ruffiana',
+    author_email='ruffiana.plays@gmail.com',
+    url='https://github.com/ruffiana/fuck_machine',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
